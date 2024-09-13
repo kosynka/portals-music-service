4d62f5c2de4d10856dae9fa28d6a07420f0dc89c
kosynka <sayat.kaldarbekov.00@gmail.com>
2024-09-13 23:23:27 +0500

    log for check

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"musicservice/src/api/router"
+	"os"
 )
 
 func main() {
 	router := router.SetupRoutes()
+
+	fmt.Println("Listening on port 8080 and printing Digital Ocean S3 URL " + os.Getenv("DIGITAL_OCEAN_S3_URL"))
 
 	router.Run(":8080")
 }
